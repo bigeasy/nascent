071d4b32acc9bc10b6be3aa38271de24628a7169
Alan Gutierrez <alan@prettyrobots.com>
2017-01-26 10:40:22 -0600

    Fix require path case sensitivity typo.

--- conduit/t/basin.queue.t.js
+++ conduit/t/basin.queue.t.js
@@ -1,7 +1,7 @@
 require('proof/redux')(1, require('cadence')(prove))
 
 function prove (async, assert) {
-    var Basin = { Queue: require('../basin.Queue') }
+    var Basin = { Queue: require('../basin.queue') }
     var queue = new Basin.Queue({
         enqueue: function (envelope, callback) {
             assert(envelope, { body: 1 }, 'enqueue')
