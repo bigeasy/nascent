96c9e8b1b42077b02bb02ed8520f72d429c62630
Alan Gutierrez <alan@prettyrobots.com>
2017-01-27 16:57:13 -0600

    Wrote Docco for Jacket.

--- jacket/jacket.js
+++ jacket/jacket.js
@@ -1,13 +1,29 @@
+// Progressive parsing of a newline delimited stream of JSON objects.
+
+// <hr>
+// Requirements.
+
+//
 var StringDecoder = require('string_decoder').StringDecoder
 
+// <hr>
+// Create a new JSON object parser.
+
+//
 function Jacket () {
-    this._lines = []
     this.object = null
     this.complete = false
     this.valid = false
+    this._lines = []
     this._decoder = new StringDecoder('utf8')
 }
 
+// <hr>
+// We're going to convert a parse exception into flags, but as I write this
+// docco I immediately prefer the notion of raising a wrapped exception that can
+// be caught by it's message, i.e. using Interrupt.
+
+//
 Jacket.prototype._json = function () {
     try {
         this.object = JSON.parse(this.json)
@@ -16,6 +32,10 @@ Jacket.prototype._json = function () {
     }
 }
 
+// <hr>
+// Parse the `buffer` at the range given by `start` and `end`.
+
+//
 Jacket.prototype.parse = function (buffer, start, end) {
     for (var i = start; i < end; i++) {
         if (buffer[i] == 0xa) {
@@ -30,4 +50,6 @@ Jacket.prototype.parse = function (buffer, start, end) {
     return end
 }
 
+// Export as constructor function.
 module.exports = Jacket
+
