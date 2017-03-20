be848bf18bff5d602fbbc1de619c9bdf357a0128
Alan Gutierrez <alan@prettyrobots.com>
2017-03-19 20:50:44 -0500

    Remove all graduated modules.

--- coalesce/t/coalesce.t.js
+++
@@ -1,7 +0,0 @@
-require('proof/redux')(2, prove)
-
-function prove (assert) {
-    var coalesce = require('..')
-    assert(coalesce(null, null), null, 'null')
-    assert(coalesce(null, (function () {})(), 1), 1, 'coalesced')
-}
