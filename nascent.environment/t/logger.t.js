0801f7ec5c12e89f7adf20f4912298ab48852ba7
Alan Gutierrez <alan@prettyrobots.com>
2018-06-16 13:06:56 -0500

    Assert logging results in test.

--- nascent.environment/t/logger.t.js
+++ nascent.environment/t/logger.t.js
@@ -2,6 +2,8 @@ require('proof')(1, prove)
 
 function prove (okay) {
     var logger = require('../logger')
+    var queue = []
+    require('prolific.resolver').sink.queue = queue
     logger({
         url: 'http://127.0.0.1:8080/bad',
         error: new Error('bad'),
@@ -10,5 +12,10 @@ function prove (okay) {
     logger({
         url: 'http://127.0.0.1:8080/bad'
     })
-    okay(true, 'need to write prolific.test')
+    console.log(queue)
+    okay(queue.map(function (entry) {
+        return entry.qualified
+    }), [
+        'environmentd#environmentd', 'environmentd#request'
+    ], 'logged')
 }
