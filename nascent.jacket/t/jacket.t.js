774ce7e0cbe83c8f294a4718eafd2b9cfe24ed32
Alan Gutierrez <alan@prettyrobots.com>
2018-11-25 18:40:46 -0600

    Rename `assert` to `okay`.

--- nascent.jacket/t/jacket.t.js
+++ nascent.jacket/t/jacket.t.js
@@ -1,16 +1,16 @@
 require('proof')(4, prove)
 
-function prove (assert) {
+function prove (okay) {
     var Jacket = require('..')
-    assert(typeof Jacket == 'function', 'require')
+    okay(typeof Jacket == 'function', 'require')
     var json = { name: '👻' }
     var packet = new Buffer(JSON.stringify(json) + '\n')
     var jacket = new Jacket
     var start = jacket.parse(packet, 0, 12)
-    assert(start, 12, 'start')
+    okay(start, 12, 'start')
     jacket.parse(packet, start, packet.length)
-    assert(jacket.object, json, 'parsed')
+    okay(jacket.object, json, 'parsed')
     var jacket = new Jacket()
     jacket.parse(new Buffer('{\n'))
-    assert(!jacket.valid, 'invalid json')
+    okay(!jacket.valid, 'invalid json')
 }
